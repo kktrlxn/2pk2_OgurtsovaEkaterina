0cec690122820cf41e42c818fd9b70c386e0df53
Ekaterina Ogurtsova <144219783+kktrlxn@users.noreply.github.com>
2023-09-09 17:49:52 +0500

    Program.cs

--- PZ_01/Program.cs
+++ PZ_01/Program.cs
@@ -10,37 +10,37 @@ namespace PZ_01
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите число a(пи=pi): ");
-            string aInput = Console.ReadLine();
-            Console.Write("Введите число b(пи=pi): ");
-            string bInput = Console.ReadLine();
-            Console.Write("Введите число c(пи=pi): ");
-            string cInput = Console.ReadLine(); //пользователь вводит три числа
-
             double a, b, c; //объявление переменных
-            //проверка условия для ввода числа пи
-            if (aInput.ToLower() == "pi")
+            Console.Write("Введите число a(пи=pi): "); //ввод числа а
+            string aInput = Console.ReadLine();
+            if (aInput.ToLower() == "pi") //проверка условия для расчетов  числом пи
             {
                 Console.Write("Введите знаменатель числа pi(при его отсутствии писать 1): ");
                 double x = Convert.ToDouble(Console.ReadLine());
                 a = Math.PI / x;
             }
             else { a = double.Parse(aInput); }
-            if (bInput.ToLower() == "pi")
+
+            Console.Write("Введите число b(пи=pi): "); //ввод числа b
+            string bInput = Console.ReadLine();
+            if (bInput.ToLower() == "pi") //проверка условия для расчетов  числом пи
             {
                 Console.Write("Введите знаменатель числа pi(при его отсутствии писать 1): ");
                 double z = Convert.ToDouble(Console.ReadLine());
                 b = Math.PI / z;
             }
             else { b = double.Parse(bInput); }
-            if (cInput.ToLower() == "pi")
+
+            Console.Write("Введите число c(пи=pi): "); //ввод числа с
+            string cInput = Console.ReadLine();
+            if (cInput.ToLower() == "pi") //проверка условия для расчетов  числом пи
             {
                 Console.Write("Введите знаменатель числа pi(при его отсутствии писать 1): ");
                 double y = Convert.ToDouble(Console.ReadLine());
                 c = Math.PI / y;
             }
             else { c = double.Parse(cInput); }
-
+            
             double result1, result2, result; //объявление переменных
             result1 = Convert.ToDouble((Math.Log(Math.Pow(b, -(Math.Sqrt(Math.Abs(a)))))) * (a - b / 2));//выполнение первого действия
             result2 = Convert.ToDouble(Math.Pow(Math.Sin(c), 2) * Math.Atan(c));//выполнение второго действия
