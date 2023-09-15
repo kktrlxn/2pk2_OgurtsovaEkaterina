85cf7d0cc63735bf19f261b6c8c004729fd70d78
Ekaterina Ogurtsova <144219783+kktrlxn@users.noreply.github.com>
2023-09-15 18:23:54 +0500

    Program.cs

--- PZ_02/Program.cs
+++ PZ_02/Program.cs
@@ -4,24 +4,24 @@
     {
         static void Main(string[] args)
         {
-            double b, c, e, d;
-            Console.Write("Введите число b: ");
+            double b, c, e, d; //определение переменных
+            Console.Write("Введите число b: "); //ввод действительного числа
             b = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите целое число a: ");
+            Console.Write("Введите целое число a: "); //ввод целого числа
             int a = Convert.ToInt32(Console.ReadLine());
-            if ( a < 0 )
+            if ( a < 0 ) //проверка условия у числа а
             {
-                c = (a - (b * (Math.Pow(a, 3)) / b));
+                c = (a - (b * (Math.Pow(a, 3)) / b)); //вычисления
             }
-            else { c = (a * b - 10); }
-            if (c <= 5)
+            else { c = (a * b - 10); } //обратное условие
+            if (c <= 5) //проверка условия числа c 
             {
-                d = (Math.Sin(2 * a) + 2 * c);
+                d = (Math.Sin(2 * a) + 2 * c); //вычисления
             }
-            else { d = (Math.Pow(Math.Cos((a - b) / (c - a)), 2)); }
+            else { d = (Math.Pow(Math.Cos((a - b) / (c - a)), 2)); } //обратное условие
 
-            e = ((2 * b + 3 * d - 11 * a * c) / 10);
-            Console.WriteLine(e);
+            e = ((2 * b + 3 * d - 11 * a * c) / 10); //вычисления
+            Console.WriteLine(e); //вывод ответа
         }
     }
 }
