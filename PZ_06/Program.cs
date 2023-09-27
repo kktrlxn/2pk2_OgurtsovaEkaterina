fbcb8f0913cb79a87951771c0f4b5cd2951c7e61
катя огурцова <ogurtsova-k28@mail.ru>
2023-09-27 14:20:18 +0500

    добавлена 6 практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();//рандом
+            int[] array = new int[10];//создается массив
+            for (int i = 0; i < array.Length; i++)//каждому элементу массива присваивается случайное число от 0 до 50
+            { array[i] = rnd.Next(0, 51); }
+            Array.Sort(array, (i, j) => GetDigitsSum(i).CompareTo(GetDigitsSum(j)));// массив сортируется в порядке возрастания суммы цифр
+
+            static int GetDigitsSum(int number)// Метод для вычисления суммы цифр числа
+            {
+                int sum = 0;
+                while (number != 0)
+                { sum += number % 10; number /= 10; }
+                return sum;
+            }
+
+            foreach (int number in array)// Вывод элементов массива с суммой их цифр
+            {
+                int sum = GetDigitsSum(number);
+                Console.WriteLine($"{number} ( сумма цифр={sum})");
+            }
+        }
+    }
+}
