b1ea96f5f29bdf03e8726c9ccde73d0758481b8f
катя огурцова <ogurtsova-k28@mail.ru>
2023-11-19 11:13:49 +0500

    добавлена 9 практика

---
+++ PZ_09/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку в нотации camelCase: "); //ввод строки в нотации camelCase
+            string input = Console.ReadLine();
+            string output = string.Empty; //создание пустой строки для того, чтобы записать в нее полученное предложение 
+
+            for (int i = 0; i < input.Length; i++)//проход по каждому символу введенной строки с помощью цикла и массива
+            {
+                char c = input[i];
+                if (char.IsUpper(c) || char.IsDigit(c))//проверка, является ли символ заглавным или цифрой
+                {
+                    output += "_" + char.ToLower(c);//если буква заглавная или это цифра, то перед ставится нижнее подчеркивание, а буква переводится в нижний регистр
+                }
+                else
+                {
+                    output += c;//иначе символ записывается в пустую строку
+                }
+            }
+            Console.WriteLine("Строка в нотации Underscore: " + output);//вывод строки в нотации Underscore
+        }
+    }
+}
