cc901ce346d0c4c8dfa8e9043be3496369974c37
катя огурцова <ogurtsova-k28@mail.ru>
2023-11-22 20:58:11 +0500

    добавлена 6 практика

--- PZ_06/Program.cs
+++ PZ_06/Program.cs
@@ -6,22 +6,25 @@
         {
             Random rnd = new Random();//рандом
             int[] array = new int[10];//создается массив
-            for (int i = 0; i < array.Length; i++)//каждому элементу массива присваивается случайное число от 0 до 50
-            { array[i] = rnd.Next(0, 51); }
-            Array.Sort(array, (i, j) => GetDigitsSum(i).CompareTo(GetDigitsSum(j)));// массив сортируется в порядке возрастания суммы цифр
-
-            static int GetDigitsSum(int number)//метод для вычисления суммы цифр числа
+            for (int i = 0; i < array.Length; i++)
+            { array[i] = rnd.Next(0, 51); }//каждому элементу массива присваивается случайное число от 0 до 50
+            int[] array2 = new int[10];//второй массив для записи в него суммы цифр числа
+            for (int i = 0; i < array2.Length; i++)//цикл для заполнения второго массива
             {
+                int number = array[i];//число берется из первого массива
                 int sum = 0;
-                while (number != 0)
-                { sum += number % 10; number /= 10; }
-                return sum;
-            }
 
-            foreach (int number in array)//вывод элементов массива с суммой их цифр
+                while (number > 0)//цикл выполняется для всех чисел больше 0
+                {
+                    sum += number % 10;//сумма будет равна сумме остатка от деления на 10 и числа поделенного на 10 без остатка
+                    number /= 10;
+                }
+                array2[i] = sum;//заполнение массива
+            }
+            Array.Sort(array2, array);//сортировка массива по возрастанию суммы чисел
+            for (int i = 0; i < array.Length; i++)
             {
-                int sum = GetDigitsSum(number);
-                Console.WriteLine($"{number} ( сумма цифр={sum})");
+                Console.WriteLine($"Сумма цифр числа {array[i]} равна {array2[i]}");//вывод
             }
         }
     }
